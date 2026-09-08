27c63e5a3099e79d1a614a0a0d1dfef0047318b1
gustavoLimaAraujo <gustavo.araujo@sptech.school>
2026-09-08 20:32:46 -0300

    fix: alterado validações e ideias do grupo

--- codigo-log/src/main/java/school/sptech/Main.java
+++ codigo-log/src/main/java/school/sptech/Main.java
@@ -6,50 +6,57 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        // Específicando o formato da saída
         DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-        // Importando a classe LocalDateTime
-        String dataHora = LocalDateTime.now().format(formato);
+        Integer parametroCpu = 80;
+        Integer parametroMemoriaRam = 80;
+        Integer parametroDisco = 80;
 
         try {
+            System.out.println(LocalDateTime.now().format(formato) + " [INFO] initiated the capture - The user initiated the capture of data from the server.");
+            Thread.sleep(2000);
+
             while (true) {
-                Integer porcentagemCpu = ThreadLocalRandom.current().nextInt(50, 101);
-                Integer porcentagemRam = ThreadLocalRandom.current().nextInt(50, 101);
-                Integer porcentagemDisco = ThreadLocalRandom.current().nextInt(50, 101);
+                Integer porcentagemCpu = ThreadLocalRandom.current().nextInt(40, 101);
+                Integer porcentagemRam = ThreadLocalRandom.current().nextInt(40, 101);
+                Integer porcentagemDisco = ThreadLocalRandom.current().nextInt(40, 101);
 
                 Integer erroBucket = ThreadLocalRandom.current().nextInt(0, 6);
 
                 if (erroBucket.equals(1)) {
-                    System.out.println(dataHora + " [ERROR] bucket error - Incorrect bucket query or a deeper-seated error");
-                    break;
+                    Integer continuarError = ThreadLocalRandom.current().nextInt(1, 5);
+
+                    for(Integer i = 0; i < continuarError; i++){
+                        System.out.println(LocalDateTime.now().format(formato) + " [ERROR] bucket error - Incorrect bucket insertion or a deeper-seated error");
+                        Thread.sleep(2000);
+                    }
 
                 } else {
-                    if (porcentagemCpu >= 80) {
-                        System.out.println(dataHora + " [WARN] excessive CPU usage - The CPU is using approx " + porcentagemCpu + "%");
+                    if (porcentagemCpu >= parametroCpu) {
+                        System.out.println(LocalDateTime.now().format(formato) + " [WARN] excessive CPU usage - The CPU is using approx " + porcentagemCpu + "%");
                     } else {
-                        System.out.println(dataHora + " [INFO] bucket record - A CPU log was created with " + porcentagemCpu + "%");
+                        System.out.println(LocalDateTime.now().format(formato) + " [INFO] bucket record - A CPU log was created with " + porcentagemCpu + "%");
                     }
 
 
-                    Thread.sleep(3000);
+                    Thread.sleep(2000);
 
 
-                    if (porcentagemRam >= 80) {
-                        System.out.println(dataHora + " [WARN] excessive RAM usage - The RAM is using approx " + porcentagemRam + "%");
+                    if (porcentagemRam >= parametroMemoriaRam) {
+                        System.out.println(LocalDateTime.now().format(formato) + " [WARN] excessive RAM usage - The RAM is using approx " + porcentagemRam + "%");
                     } else {
-                        System.out.println(dataHora + " [INFO] bucket record - A RAM log was created with " + porcentagemRam + "%");
+                        System.out.println(LocalDateTime.now().format(formato) + " [INFO] bucket record - A RAM log was created with " + porcentagemRam + "%");
                     }
 
-                    Thread.sleep(3000);
+                    Thread.sleep(2000);
 
-                    if (porcentagemDisco >= 80) {
-                        System.out.println(dataHora + " [WARN] excessive Storage usage - The Storage is using approx " + porcentagemDisco + "%");
+                    if (porcentagemDisco >= parametroDisco) {
+                        System.out.println(LocalDateTime.now().format(formato) + " [WARN] excessive Storage usage - The Storage is using approx " + porcentagemDisco + "%");
                     } else {
-                        System.out.println(dataHora + " [INFO] bucket record - A Storage log was created with " + porcentagemDisco + "%");
+                        System.out.println(LocalDateTime.now().format(formato) + " [INFO] bucket record - A Storage log was created with " + porcentagemDisco + "%");
                     }
 
-                    Thread.sleep(5000);
+                    Thread.sleep(2000);
                 }
             }
         } catch (InterruptedException e) {
